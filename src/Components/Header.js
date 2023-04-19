39e18702a7a1be701767a3e39bde9db72e092ef1
Nelani Maluka <malukanelani@gmail.com>
2023-04-19 21:02:09 +0200

    addeed the headers styling and title

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Header.css"
+
+function Header() {
+    return (
+    <header className="background">
+        <div>
+            <img src="" alt="Logo"/>
+            <h2>Personal Budgetting APP</h2>
+        </div>
+    </header>
+    )
+}
+
+export default Header;
